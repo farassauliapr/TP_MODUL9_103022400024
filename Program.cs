2167ee0c40d8a3d29fd11a27b8513eb3ba718604
farassauliapr <farasauliapr@gmail.com>
2026-04-26 20:47:07 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TP_MODUL9_103022400024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
